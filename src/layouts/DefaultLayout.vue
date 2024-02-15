c633cc444d87939a1c6bac3b91a7bca5cdc9acb3
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-02-15 17:43:25 +0300

    add the default header to the Default Layout

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,5 +1,8 @@
 <template>
+  <HeaderDefault />
   <slot />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HeaderDefault from '@/share/widgets/HeaderDefault.vue';
+</script>
