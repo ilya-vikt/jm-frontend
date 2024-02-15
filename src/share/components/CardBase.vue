09276656ebce0b6e0bd14e867bcb8d50ac8dc93d
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-02-15 18:46:31 +0300

    add CardBase component. It is the basis for different cards, blocks, etc...

---
+++ src/share/components/CardBase.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card {
+  background-color: var(--cl-block);
+  padding: $padding-v $padding-h;
+  border-radius: $border-radius;
+}
+</style>
