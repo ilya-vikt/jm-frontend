a636dec590654b679f5d7c9a06eae69f40b1e739
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-02-15 11:33:28 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <AppLayout>
+    <RouterView />
+  </AppLayout>
+</template>
+
+<style lang="scss">
+@import './assets/styles/style.scss';
+</style>
