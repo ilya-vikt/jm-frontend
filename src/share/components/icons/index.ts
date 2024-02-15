f98521409d2701a26e9b1051a4fb8b6ff93b348c
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-02-15 17:39:34 +0300

    add icons for the Default header

---
+++ src/share/components/icons/index.ts
@@ -0,0 +1,9 @@
+import BurgerIcon from './BurgerIcon.vue';
+import CartIcon from './CartIcon.vue';
+import HeartIcon from './HeartIcon.vue';
+import InfoIcon from './InfoIcon.vue';
+import ParcelIcon from './ParcelIcon.vue';
+import UserIcon from './UserIcon.vue';
+import CatalogIcon from './CatalogIcon.vue';
+
+export { BurgerIcon, CartIcon, HeartIcon, InfoIcon, ParcelIcon, UserIcon, CatalogIcon };
