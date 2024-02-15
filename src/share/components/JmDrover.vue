d4ca2621c70839e4cc72eb643088468ef13fa1ae
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-02-15 18:37:41 +0300

    add JmDrover component. It is a common component to show splash blocks such as menu, categories etc...

---
+++ src/share/components/JmDrover.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    v-if="visible"
+    class="drover"
+    @click.self="$emit('closeDrover')"
+  >
+    <div class="container">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  visible: boolean;
+}>();
+
+defineEmits<{
+  closeDrover: [];
+}>();
+</script>
+
+<style scoped lang="scss">
+.drover {
+  position: fixed;
+  inset: 0;
+  top: var(--header-height);
+  padding: $padding-v $padding-h;
+  background-color: rgba(0, 0, 0, 0.3);
+}
+</style>
