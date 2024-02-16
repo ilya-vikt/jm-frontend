929126d7dd80a440f101e08fa5a194e2f3e5fefb
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-02-16 11:59:51 +0300

    add ArrowLeft and Close icons

--- src/share/components/icons/index.ts
+++ src/share/components/icons/index.ts
@@ -5,5 +5,17 @@ import InfoIcon from './InfoIcon.vue';
 import ParcelIcon from './ParcelIcon.vue';
 import UserIcon from './UserIcon.vue';
 import CatalogIcon from './CatalogIcon.vue';
+import ArrowLeftIcon from './ArrowLeftIcon.vue';
+import CloseIcon from './CloseIcon.vue';
 
-export { BurgerIcon, CartIcon, HeartIcon, InfoIcon, ParcelIcon, UserIcon, CatalogIcon };
+export {
+  BurgerIcon,
+  CartIcon,
+  HeartIcon,
+  InfoIcon,
+  ParcelIcon,
+  UserIcon,
+  CatalogIcon,
+  ArrowLeftIcon,
+  CloseIcon
+};
