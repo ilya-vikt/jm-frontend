4b3163f49ae8486c01c8b013e34d16c45673cd90
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-02-25 12:53:06 +0300

    add padding to the Default layout

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,8 +1,16 @@
 <template>
   <HeaderDefault />
-  <slot />
+  <main>
+    <slot />
+  </main>
 </template>
 
 <script setup lang="ts">
 import HeaderDefault from '@/share/widgets/HeaderDefault.vue';
 </script>
+
+<style scoped lang="scss">
+main {
+  padding: calc(var(--header-height) + $padding-v) $padding-v $padding-h;
+}
+</style>
