d55a55f547f0a1657be6b257d06e31fe8102c374
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-02-26 21:38:22 +0300

    add an article block to the about page

--- src/about/views/AboutView.vue
+++ src/about/views/AboutView.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>About page</div>
+  <CardBase class="about">
+    <div class="about__inner">
+      <AboutArticle />
+    </div>
+  </CardBase>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CardBase from '@/share/components/CardBase.vue';
+import AboutArticle from '@/about/blocks/AboutArticle.vue';
+</script>
+
+<style scoped lang="scss">
+.about {
+  &__inner {
+    display: flex;
+    flex-direction: column;
+    gap: $gap-base * 4;
+    max-width: 1000px;
+    margin: 0 auto;
+  }
+}
+</style>
