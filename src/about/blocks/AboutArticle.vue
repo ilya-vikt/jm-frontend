a3016d8d406ed21e9ce1bc57d258df2b01da9012
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-02-27 09:02:58 +0300

    add links to github repos in Article block on the About page

--- src/about/blocks/AboutArticle.vue
+++ src/about/blocks/AboutArticle.vue
@@ -6,6 +6,29 @@
     <h2>Техническое описание</h2>
     <p>Joky Market это FullStack приложение</p>
     <p>Frontend написан с использованием фреймворка Vue 3</p>
+    <p>
+      <OuterLink
+        :icon="GithubIcon"
+        href="https://github.com/ilya-vikt/jm-frontend"
+        underline
+        target="_blank"
+        >Код на GitHub</OuterLink
+      >
+    </p>
     <p>Backend написан с использованием фреймворка Nest</p>
+    <p>
+      <OuterLink
+        :icon="GithubIcon"
+        href="https://github.com/ilya-vikt/jm-backend"
+        underline
+        target="_blank"
+        >Код на GitHub</OuterLink
+      >
+    </p>
   </article>
 </template>
+
+<script setup lang="ts">
+import GithubIcon from '@/share/components/icons/GithubIcon.vue';
+import OuterLink from '@/about/components/OuterLink.vue';
+</script>
