a2e20d8a62c907568b57b87a1f6bb187549ce93f
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-03-08 10:27:49 +0300

    add FilterMultiselect component. This component uses in a filter panel for type of MULTILIST filters

---
+++ src/catalog/components/FilterMultiselect.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="multiselect"></div>
+  <p
+    v-if="filterData.caption"
+    class="multiselect__caption"
+  >
+    {{ filterData.caption }}
+  </p>
+  <ul class="multiselect__list">
+    <li
+      v-for="checkbox in filterData.possibleValues"
+      :key="checkbox.id"
+      class="multiselect__item"
+    >
+      <FilterCheckbox
+        v-model="modelValue"
+        :value="checkbox.id"
+        :filter-data="checkbox.value"
+      />
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import type { FilterData } from '@/catalog/types';
+import FilterCheckbox from './FilterCheckbox.vue';
+defineProps<{
+  filterData: FilterData;
+}>();
+
+const modelValue = defineModel<number[]>({ required: false });
+</script>
+
+<style scoped lang="scss">
+.multiselect {
+  &__caption {
+    margin-bottom: 5px;
+  }
+}
+</style>
