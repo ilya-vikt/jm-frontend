47efd27e8bad743f0af6f7d4853a555f4dfe2513
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-03-08 12:57:03 +0300

    add JmLoader component. This is a block that is used to create skeleton loaders and image lazy load

---
+++ src/share/components/JmLoader.vue
@@ -0,0 +1,22 @@
+<template><div class="loader"></div></template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.loader {
+  filter: brightness(50%);
+  background-color: var(--cl-hover);
+  animation: shine 3s linear infinite forwards;
+}
+
+@keyframes shine {
+  0%,
+  100% {
+    filter: brightness(100%);
+  }
+
+  50% {
+    filter: brightness(75%);
+  }
+}
+</style>
