639b25e82cac5a248e984f3cf93012cb08cbf90d
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-03-08 23:31:00 +0300

    add ProductsNotFound component. This component is displayed when no products are found

---
+++ src/catalog/components/ProductsNotFound.vue
@@ -0,0 +1,32 @@
+<template>
+  <CardBase class="not-found">
+    <NotFoundIcon class="not-found__icon" />
+    <div class="not-found__text">
+      <p>Товаров, соответствующих запросу, не найдено.</p>
+      <p>Попробуйте изенить фильтры</p>
+    </div>
+    <JmLoader />
+  </CardBase>
+</template>
+
+<script setup lang="ts">
+import CardBase from '@/share/components/CardBase.vue';
+import JmLoader from '@/share/components/JmLoader.vue';
+import { NotFoundIcon } from '@/share/components/icons';
+</script>
+
+<style scoped lang="scss">
+.not-found {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: $gap-base;
+
+  &__icon {
+    flex-shrink: 0;
+  }
+  &__text {
+    font-size: 1.5rem;
+  }
+}
+</style>
