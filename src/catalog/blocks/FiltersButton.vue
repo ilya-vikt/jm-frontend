66b597f9bd2b20ac2e8a6c129c741ad42a324240
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-03-08 23:41:13 +0300

    add FiltersButton component. This component displays the button to toggle visibility of a drover width filter menu on the search page in compact mode (isCompact breakpoint)

---
+++ src/catalog/blocks/FiltersButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <JmButton
+    v-if="isCompact && $route.params.secondaryCategory"
+    class="header-default__catalog"
+    has-hover
+    icon-size="24"
+    :icon="FilterIcon"
+    @click="toggleFiltersPanel"
+  />
+</template>
+
+<script setup lang="ts">
+import { useBreackpoints } from '@/share/composable/useBreackpoints';
+import { useDroverController } from '@/share/composable/useDroverController';
+import { FilterIcon } from '@/share/components/icons';
+import JmButton from '@/share/components/JmButton.vue';
+import { FiltersPanelDroverName } from '@/catalog/constants';
+const { toggleDrover } = useDroverController();
+const { isCompact } = useBreackpoints();
+
+const toggleFiltersPanel = () => {
+  toggleDrover(FiltersPanelDroverName);
+};
+</script>
