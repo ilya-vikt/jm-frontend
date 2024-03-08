49266f1db36b9a283482e3038d352017c61170c9
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-03-08 13:05:49 +0300

    add isUltraWide breakpoint to the useBreakpoints composable function. UltraWidt is min-width 1921px

--- src/share/composable/useBreackpoints.ts
+++ src/share/composable/useBreackpoints.ts
@@ -1,15 +1,17 @@
-import { useMediaQuery } from '@vueuse/core'
+import { useMediaQuery } from '@vueuse/core';
 
-const isMobile = useMediaQuery('(max-width: 639px)')
-const isTablet = useMediaQuery('(min-width: 640px) and (max-width: 1024px)')
-const isLaptop = useMediaQuery('(min-width: 1025px) and (max-width: 1279px)')
-const isDesktop = useMediaQuery('(min-width: 1280px)')
-const isCompact = useMediaQuery('(max-width: 1024px)')
+const isMobile = useMediaQuery('(max-width: 639px)');
+const isTablet = useMediaQuery('(min-width: 640px) and (max-width: 1024px)');
+const isLaptop = useMediaQuery('(min-width: 1025px) and (max-width: 1279px)');
+const isDesktop = useMediaQuery('(min-width: 1280px)');
+const isUltraWide = useMediaQuery('(min-width: 1921px)');
+const isCompact = useMediaQuery('(max-width: 1024px)');
 
 export const useBreackpoints = () => ({
   isDesktop,
   isLaptop,
   isTablet,
   isMobile,
-  isCompact
-})
+  isCompact,
+  isUltraWide
+});
