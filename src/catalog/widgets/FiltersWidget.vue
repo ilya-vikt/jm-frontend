c7170aedf8d206a235dc9b561d2be647d2cd6ddc
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-03-08 23:29:08 +0300

    add The FiltersWidget component. This component integrates FiltersPanel and CategoryPanel

---
+++ src/catalog/widgets/FiltersWidget.vue
@@ -0,0 +1,25 @@
+<template>
+  <JmDrover
+    :drover-name="FiltersPanelDroverName"
+    class="filters-widget"
+    :disabled="!isCompact"
+  >
+    <FiltersPanel
+      v-if="$route.params.secondaryCategory"
+      class="filters-widget__block"
+    />
+    <CategoriesPanel
+      v-else
+      class="filters-widget__block"
+    />
+  </JmDrover>
+</template>
+<script setup lang="ts">
+import CategoriesPanel from '@/catalog/blocks/CategoriesPanel.vue';
+import FiltersPanel from '@/catalog/blocks/FiltersPanel.vue';
+import JmDrover from '@/share/components/JmDrover.vue';
+import { FiltersPanelDroverName } from '@/catalog/constants';
+import { useBreackpoints } from '@/share/composable/useBreackpoints';
+
+const { isCompact } = useBreackpoints();
+</script>
