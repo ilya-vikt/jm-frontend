3a2ac0d869f31f94754a81156abf5fb3df480cd1
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-03-08 23:34:54 +0300

    remake ProductsList component

--- src/catalog/widgets/ProductsList.vue
+++ src/catalog/widgets/ProductsList.vue
@@ -1,7 +1,10 @@
 <template>
-  <ul class="products-list">
+  <ul
+    v-if="!isProductsFetching"
+    class="products-list"
+  >
     <li
-      v-for="product in productsList"
+      v-for="product in products"
       :key="product.id"
       class="products-list__card"
     >
@@ -10,35 +13,55 @@
       </RouterLink>
     </li>
   </ul>
+  <ul
+    v-if="isProductsFetching"
+    class="products-list__sceleton"
+  >
+    <li
+      v-for="idx in 4"
+      :key="idx"
+    >
+      <ProductCardSceleton
+        :style="{
+          '--idx': idx
+        }"
+      />
+    </li>
+  </ul>
 </template>
 
 <script setup lang="ts">
 import ProductCard from '@/catalog/blocks/ProductCard.vue';
-import { useProducts } from '@/catalog/composable/useProducts';
+import ProductCardSceleton from '@/catalog/blocks/ProductCardSceleton.vue';
 import { controllerName } from '@/catalog/constants';
-const { productsList } = useProducts();
+import { useProducts } from '@/catalog/composable/useProducts';
 
-const urlBase = `${controllerName}/product`;
+const { products, isProductsFetching } = useProducts();
+
+const urlBase = `/${controllerName}/product`;
 </script>
 
 <style scoped lang="scss">
-.products-list {
+.products-list,
+.products-list__sceleton {
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(3, minmax(250px, 1fr));
   gap: $gap-base;
   max-width: 1400px;
+  width: 100%;
   margin: 0 auto;
 }
 
 .is-laptop .products-list {
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(2, minmax(250px, 1fr));
 }
 
 .is-tablet .products-list {
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(2, minmax(250px, 1fr));
 }
 
 .is-mobile .products-list {
-  grid-template-columns: repeat(1, 1fr);
+  grid-template-columns: repeat(1, minmax(250px, 1fr));
 }
 </style>
+../composable/useProducts
