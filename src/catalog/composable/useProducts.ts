3720da67c2ad6cf3071bd7c93a1d0d1c589745f1
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-03-08 21:22:33 +0300

    add fetching products by search params

--- src/catalog/composable/useProducts.ts
+++ src/catalog/composable/useProducts.ts
@@ -1,24 +1,82 @@
-import type { ProductCardInput } from '@/catalog/types/products';
-import { controllerName, endpoints } from '@/catalog/constants';
-import { useFetch } from '@vueuse/core';
-import { ref } from 'vue';
+import type { FilterValue, Product } from '@/catalog/types';
+import { reactive, ref, watchEffect } from 'vue';
+import { useFetch, watchDebounced } from '@vueuse/core';
+import { endpoints } from '@/catalog/constants';
+import { useFilters } from '@/catalog/composable/useFilters';
+import { useCategories } from '@/catalog/composable/useCategories';
+
+const searchParams = reactive<{
+  searchString: string;
+  appliedFilters:
+    | {
+        id: number;
+        value: NonNullable<FilterValue>;
+      }[]
+    | null;
+  categoryId: number | null;
+}>({
+  searchString: '',
+  appliedFilters: null,
+  categoryId: null
+});
+
+const refetchIsAllow = ref(false);
+
+const { filters } = useFilters();
+const { currentPrimaryCategoryId, currentSecondaryCategoryId } = useCategories();
+
+const {
+  data: products,
+  execute: refetchProducts,
+  isFetching: isProductsFetching
+} = useFetch(endpoints.searchProducts, {
+  immediate: false
+})
+  .post(searchParams)
+  .json<Product[] | null>();
+
+const getQueryParams = () => {
+  const params = searchParams.appliedFilters
+    ? searchParams.appliedFilters.reduce<Record<string, string | null>>((acc, { id, value }) => {
+        const filter = filters.value?.find((f) => f.id === id);
+        if (filter) {
+          if (filter.type === 'BOOLEAN') {
+            acc[filter.name] = null;
+          } else {
+            acc[filter.name] = value.toString();
+          }
+        }
+        return acc;
+      }, {})
+    : {};
+
+  if (searchParams.searchString) {
+    params['search'] = searchParams.searchString;
+  }
+  return params;
+};
+
+watchEffect(
+  () =>
+    (searchParams.categoryId =
+      currentSecondaryCategoryId.value ?? currentPrimaryCategoryId.value ?? null)
+);
+
+watchDebounced(
+  searchParams,
+  () => {
+    if (!refetchIsAllow.value) return;
+    refetchProducts();
+  },
+  { debounce: 100 }
+);
 
 export const useProducts = () => {
-  const page = ref(1);
-  const limit = ref(20);
-
-  const {
-    data: productsList,
-    isFetching,
-    error
-  } = useFetch(
-    `${import.meta.env.VITE_API_URL}/${controllerName}/${endpoints.getProductCards}?page=${page.value}&limit=${limit.value}`
-  ).json<ProductCardInput[] | null>();
   return {
-    isFetching,
-    error,
-    page,
-    limit,
-    productsList
+    searchParams,
+    products,
+    isProductsFetching,
+    refetchIsAllow,
+    getQueryParams
   };
 };
