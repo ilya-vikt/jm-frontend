8cc2d00b15228b9ed21b4496da7f01c2c18f4703
Ilya Viktorov <ilya-vikt@yandex.ru>
2024-03-08 23:42:46 +0300

    add CategoriesMenuButton, SearchBar, FiltersButton components to the catalog module interface

--- src/catalog/index.ts
+++ src/catalog/index.ts
@@ -1,3 +1,5 @@
-export * from '@/catalog/utils/categoriesMenu';
 import CategoriesMenu from '@/catalog/widgets/CategoriesMenu.vue';
-export { CategoriesMenu };
+import CategoriesMenuButton from '@/catalog/blocks/CategoriesMenuButton.vue';
+import SearchBar from '@/catalog/blocks/SearchBar.vue';
+import FiltersButton from '@/catalog/blocks/FiltersButton.vue';
+export { CategoriesMenu, CategoriesMenuButton, SearchBar, FiltersButton };
